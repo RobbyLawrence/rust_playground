1b7cc325336d5c575801fbf0fa499b3084a1ba86
Robert Lawrence <rwl19291@gmail.com>
2025-05-23 21:06:45 -0400

    cleaning up

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,11 +1,14 @@
-use std::io;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
 
-fn main() {
-    let mut s = String::from("hello");
+fn read_graph_from_file(String file_path) {
 
-    let r1 = &mut s;
-    println!("{}", r1);
-    let r2 = &mut s;
-
-    println!("{}, {}", r1, r2);
+}
+fn main() -> io::Result<()> {
+    let file_path = "k5.dim";
+    let (num_vertices, adjmatrix) = read_graph_from_file(file_path)?;
+    println!("Number of vertices: {}", num_vertices);
+    println!("Adjacency Matrix:");
+    Ok(())
 }
