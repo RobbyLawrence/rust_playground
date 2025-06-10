25f02150196d65b9815b7ed52a5b6e7839b9600f
Robert Lawrence <rwl19291@gmail.com>
2025-06-09 22:37:27 -0400

    updating repo

--- projects/fibonacci/src/main.rs
+++ projects/fibonacci/src/main.rs
@@ -1,34 +1,16 @@
-use std::io;
-
-fn fibonacci(n: u32) -> u64 {
-    if n == 1 || n == 0 {
-        return 1;
-    } else {
-        let mut prev = 1;
-        let mut curr = 1;
-        for _ in 2..=n {
-            let next = prev + curr;
-            prev = curr;
-            curr = next;
-        }
-        return curr;
-    }
-}
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    println!("Enter a number: ");
-    let num = loop {
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-        match input.trim().parse::<u32>() {
-            Ok(val) => {
-                break val;
-            }
-            Err(_) => continue,
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
         }
-    };
-    let fib = fibonacci(num - 1);
-    println!("{fib}");
+    });
+    for i in 1..5 {
+        println!("hi number {i} from the main thread");
+        thread::sleep(Duration::from_millis(1));
+    }
+    handle.join().unwrap();
 }
